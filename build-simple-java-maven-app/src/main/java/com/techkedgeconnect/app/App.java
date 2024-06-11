2b37b259629ee078e804b84b469081b3b4d46adc
edgeconnect-codecommit-user01-at-381492305313 <edgeconnect-codecommit-user01-at-381492305313>
2024-06-11 17:43:38 +0100

    build simple java app with maven - 1st commit

---
+++ build-simple-java-maven-app/src/main/java/com/techkedgeconnect/app/App.java
@@ -0,0 +1,19 @@
+package com.techkedgeconnect.app;
+
+/**
+ * Welcome to Techkedgeconnect, your one stop hub for evrything Cloud and DevOps Career Development!
+ */
+public class App {
+
+    private static final String MESSAGE = "Welcome to Techkedgeconnect, your one stop hub for evrything Cloud and DevOps Career Development!";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println(MESSAGE);
+    }
+
+    public String getMessage() {
+        return MESSAGE;
+    }
+}
---
+++ build-simple-java-maven-app/src/test/java/com/techkedgeconnect/app/AppTest.java
@@ -0,0 +1,25 @@
+package com.techkedgeconnect.app;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    @Test
+    public void testAppConstructor() {
+        App app1 = new App();
+        App app2 = new App();
+        assertEquals(app1.getMessage(), app2.getMessage());
+    }
+
+    @Test
+    public void testAppMessage()
+    {
+        App app = new App();
+        assertEquals("Welcome to Techkedgeconnect, your one stop hub for evrything Cloud and DevOps Career Development!", app.getMessage());
+    }
+}
